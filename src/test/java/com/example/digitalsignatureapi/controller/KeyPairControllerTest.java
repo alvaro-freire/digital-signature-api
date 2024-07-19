4580eef843d0b0f136c0ec32b43200e9610f2933
Álvaro Freire Ares <alvaro@innogando.com>
2024-07-19 19:15:23 +0200

    apply formatter

--- src/test/java/com/example/digitalsignatureapi/controller/KeyPairControllerTest.java
+++ src/test/java/com/example/digitalsignatureapi/controller/KeyPairControllerTest.java
@@ -26,8 +26,8 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
